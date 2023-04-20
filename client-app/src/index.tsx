68aaef3dbc1bdb003f46453918b0c1cd996b86ca
razvan.tanase2309 <razvan.tanase2309@stud.acs.upb.ro>
2023-04-20 06:00:31 +0300

    Introduced State management with MobX

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/style.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { StoreContext, store } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
